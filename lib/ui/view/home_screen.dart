0c67ba4c376f40aa4bcd0a3962ac9e05767fe522
jorgecid <jorge.cc91@gmail.com>
2023-12-14 20:54:26 -0400

    challenge done.

--- lib/ui/view/home_screen.dart
+++ lib/ui/view/home_screen.dart
@@ -15,6 +15,7 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
+
 class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
